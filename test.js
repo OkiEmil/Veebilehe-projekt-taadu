6d1477be046e884edaca96921a77687c88d52151
Kerreth Arula <kerreth@ut.ee>
2025-11-24 16:55:18 +0200

    Slaideri tehnoloogia

--- test.js
+++ test.js
@@ -15,10 +15,24 @@ function prevQuestion(current) {
 function calculateResult() {
   let scores = { A: 0, B: 0, C: 0 };
 
-  const formData = new FormData(document.getElementById("quizForm"));
-  for (let value of formData.values()) {
-    scores[value]++;
+  function skoorTäheks(index) {
+    return String.fromCharCode(65 + index);
   }
+
+  const formData = new FormData(document.getElementById("quizForm"));
+  for (let [name, value] of formData.entries()) {
+    if (name === "slaider") {
+      const numValue = parseInt(value, 10);
+      const täht = skoorTäheks(numValue);
+      if (scores.hasOwnProperty(täht)) {
+        scores[täht]++;
+      }
+    } 
+    else {
+      scores[value]++;
+      }
+    }
+  
 
   let highest = Object.keys(scores).reduce((a, b) =>
     scores[a] > scores[b] ? a : b
